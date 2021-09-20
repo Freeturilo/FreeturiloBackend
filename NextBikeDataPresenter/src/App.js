d7f41f530ddb098fb406d0c92ab22e036fe1bea1
mryll <m.ryll@ap-tech.pl>
2021-09-20 14:53:39 +0200

    Dockerized

--- NextBikeDataPresenter/src/App.js
+++ NextBikeDataPresenter/src/App.js
@@ -1,24 +1,10 @@
-import * as signalR from '@microsoft/signalr';
 import './App.css';
 
 function App() {
-  var connection = new signalR.HubConnectionBuilder()
-  .withUrl("https://localhost:5001/nextBike",{
-    skipNegotiation: true,
-    transport: signalR.HttpTransportType.WebSockets
-  })
-  .build();
-
-  connection.on("UpdateStation", console.log);
-
-  connection.start()
-  .then(() => console.log("starrted"));
 
   return (
     <div className="App">
-      <button onClick={() => {
-        connection.send("UpdateStation", "Station Rakowiec", 123);
-      }}>
+      <button>
         Button
       </button>
     </div>
