1796e441261d8c393072398ce52583aae5e037ab
Mikołaj Ryll <01142207@pw.edu.pl>
2021-11-06 19:05:51 +0100

    dtos added

---
+++ FreeturiloWebApi/DTO/LocationDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace FreeturiloWebApi.DTO
+{
+    public class LocationDTO
+    {
+        public double? Longitude { get; set;}
+        public double? Latitude { get; set;}
+        public string Name { get; set;}
+    }
+}
