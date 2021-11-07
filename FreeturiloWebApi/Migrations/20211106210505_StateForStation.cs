d25103ac3a920dc03f88b24eea20dc78d44fc41c
Mikołaj Ryll <01142207@pw.edu.pl>
2021-11-07 17:26:57 +0100

    db update

---
+++ FreeturiloWebApi/Migrations/20211106210505_StateForStation.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FreeturiloWebApi.Migrations
+{
+    public partial class StateForStation : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "State",
+                table: "Stations",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "RouteJSON",
+                table: "Routes",
+                type: "text",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "State",
+                table: "Stations");
+
+            migrationBuilder.DropColumn(
+                name: "RouteJSON",
+                table: "Routes");
+        }
+    }
+}
