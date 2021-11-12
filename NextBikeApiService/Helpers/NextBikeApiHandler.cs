b0a10d79d20e1ec3c061ccc19aafe7a01cee9e6d
Mikołaj Ryll <01142207@pw.edu.pl>
2021-11-12 02:04:38 +0100

    Getting token and stations + Methods moved to helper classes

---
+++ NextBikeApiService/Helpers/NextBikeApiHandler.cs
@@ -0,0 +1,45 @@
+﻿using NextBikeDataParser;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using System.Net.Http.Headers;
+
+namespace NextBikeApiService.Helpers
+{
+    static class NextBikeApiHandler
+    {
+        private static readonly string _xsdPath = @"../NextBikeDataParser/markers.xsd";
+
+        private const string LibUrl = @"http://example.org/mr/nextbikesdata";
+        private const string UrlBase = @"https://nextbike.net/maps/nextbike-live.xml";
+        private const string UrlParameters = "?city=210";
+
+        public static markers GetNextBikeData(CancellationToken stoppingToken, ILogger logger)
+        {
+            HttpResponseMessage nextBikeResponse;
+            using (HttpClient client = new() { BaseAddress = new Uri(UrlBase) })
+            {
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/xml"));
+                nextBikeResponse = client.GetAsync(UrlParameters, stoppingToken).Result;
+            }
+
+            if (!nextBikeResponse.IsSuccessStatusCode)
+            {
+                logger.LogError($"Error when requesting nextbike.net: {nextBikeResponse.StatusCode} {nextBikeResponse.Content}");
+            }
+
+            var xmlContent = nextBikeResponse.Content.ReadAsStringAsync(stoppingToken).Result;
+            var xmlContentSplit = xmlContent.Split(new string[] { "markers" }, StringSplitOptions.None);
+            var xmlContentJoined = xmlContentSplit[0] + $"markers xmlns=\"{LibUrl}\"" + xmlContentSplit[1] +
+                                   "markers" +
+                                   xmlContentSplit[2];
+            var nextBikesData = Parser.ReadNextBikesData(xmlContentJoined, _xsdPath, LibUrl);
+            return nextBikesData;
+        }
+    }
+}
--- NextBikeApiService/Worker.cs
+++ NextBikeApiService/Worker.cs
@@ -1,83 +1,33 @@
 using System;
-using System.Collections.Generic;
-using System.IO;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Threading;
 using System.Threading.Tasks;
-using System.Xml;
-using System.Xml.Serialization;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using NextBikeDataParser;
-using Microsoft.AspNetCore.SignalR.Client;
-using FreeturiloWebApi.Models;
+
+using NextBikeApiService.Helpers;
 
 namespace NextBikeApiService
 {
     public class Worker : BackgroundService
     {
         private readonly ILogger<Worker> _logger;
-        private readonly string _xsdPath = Environment.GetEnvironmentVariable("XSD_PATH") ??  @"../NextBikeDataParser/markers.xsd";
-        private readonly string _hubPath = Environment.GetEnvironmentVariable("HUB_PATH") ??  @"http://localhost:5000/nextBike";
-
-        private const string LibUrl = @"http://example.org/mr/nextbikesdata";
-        private const string UrlBase = @"https://nextbike.net/maps/nextbike-live.xml";
-        private const string UrlParameters = "?city=210";
-
         private const int SecondsDelay = 3;
-
         public Worker(ILogger<Worker> logger)
         {
             _logger = logger;
         }
-
+        
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
-            var _connection = new HubConnectionBuilder()
-                .WithUrl(_hubPath)
-                .Build();
-            await _connection.StartAsync();
-            _logger.LogInformation("Connected to Hub on " + _hubPath);
-
             while (!stoppingToken.IsCancellationRequested)
             {
-                HttpResponseMessage response;
-                using (HttpClient client = new() { BaseAddress = new Uri(UrlBase) })
-                {
-                    client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/xml"));
-                    response = client.GetAsync(UrlParameters, stoppingToken).Result;
-                }
+                var token = FreeturiloApiHandler.GetToken(stoppingToken);
+                var markers = NextBikeApiHandler.GetNextBikeData(stoppingToken, _logger);
+                var freeturiloStations = FreeturiloApiHandler.GetFreeturiloStations(stoppingToken);
 
-                if (!response.IsSuccessStatusCode)
-                {
-                    _logger.LogError($"Error when requesting nextbike.net: {response.StatusCode} {response.Content}");
-                }
-
-                var xmlContent = response.Content.ReadAsStringAsync(stoppingToken).Result;
-                var xmlContentSplit = xmlContent.Split(new string[] { "markers" }, StringSplitOptions.None);
-                var xmlContentJoined = xmlContentSplit[0] + $"markers xmlns=\"{LibUrl}\"" + xmlContentSplit[1] +
-                                       "markers" +
-                                       xmlContentSplit[2];
-                var nextBikesData = Parser.ReadNextBikesData(xmlContentJoined, _xsdPath, LibUrl);
-
-                var stations = new List<Station>();
-                foreach (var place in nextBikesData.country.city.place)
-                {
-                    var station = new Station()
-                    {
-                        Id = place.uid,
-                        Lon = (double)place.lng,
-                        Lat = (double)place.lat,
-                        Name = place.name,
-                        AvailableBikes = place.bikes_available_to_rent,
-                    };
-
-                    stations.Add(station);
-                }
-
-                await _connection.InvokeAsync("UpdateStations", stations);
-                _logger.LogInformation("Status updated");
                 await Task.Delay(1000 * SecondsDelay, stoppingToken);
             }
         }
