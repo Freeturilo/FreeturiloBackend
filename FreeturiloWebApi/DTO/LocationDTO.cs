bad341a182762d5a22860e9b9ff597eeb2669e85
Mikołaj Ryll <01142207@pw.edu.pl>
2021-11-12 02:01:51 +0100

    Names for Json properties in DTO objects

--- FreeturiloWebApi/DTO/LocationDTO.cs
+++ FreeturiloWebApi/DTO/LocationDTO.cs
@@ -1,9 +1,14 @@
-﻿namespace FreeturiloWebApi.DTO
+﻿using System.Text.Json.Serialization;
+
+namespace FreeturiloWebApi.DTO
 {
     public class LocationDTO
     {
+        [JsonPropertyName("longitude")]
         public double? Longitude { get; set;}
+        [JsonPropertyName("latitude")]
         public double? Latitude { get; set;}
+        [JsonPropertyName("name")]
         public string Name { get; set;}
     }
 }
--- FreeturiloWebApi/DTO/StationDTO.cs
+++ FreeturiloWebApi/DTO/StationDTO.cs
@@ -1,10 +1,16 @@
-﻿namespace FreeturiloWebApi.DTO
+﻿using System.Text.Json.Serialization;
+
+namespace FreeturiloWebApi.DTO
 {
     public class StationDTO : LocationDTO
     {
+        [JsonPropertyName("id")]
         public int Id  { get; set; }
+        [JsonPropertyName("bikeRacks")]
         public int BikeRacks  { get; set; }
+        [JsonPropertyName("bikes")]
         public int Bikes { get; set; }
+        [JsonPropertyName("state")]
         public int State { get; set; }
     }
 }
