d408f85710e78c03d6fb82746d084fc2be27ad19
Mikołaj Ryll <01142207@pw.edu.pl>
2021-12-16 01:32:06 +0100

    Tests updated

---
+++ run_tests.bat
@@ -0,0 +1,1 @@
+cd .\FreeturiloWebApi.Test && .\generate_report.bat && cd .. && cd .\NextBikeApiService.Test && .\generate_report.bat && cd .. && cd .\NextBikeDataParser.Test && .\generate_report.bat && cd ..
